ccb805b949ee78ec6ca25093c180c9511703143d
rusky65 <->
2018-08-16 09:21:58 +0200

    #3: Service shuting down when the PC is switcing off (shut down).

--- FWService/FWService.cs
+++ FWService/FWService.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Diagnostics;
 using System.Linq;
+using System.Reflection;
 using System.ServiceProcess;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,9 @@ namespace FWService
 {
     public partial class FWService : ServiceBase
     {
+        const int SERVICE_ACCEPT_PRESHUTDOWN = 0x100;
+        const int SERVICE_CONTROL_PRESHUTDOWN = 0xf;
+
         public FWService()
         {
             InitializeComponent();
@@ -27,7 +31,27 @@ namespace FWService
             eventLogFwService.Log = "FWServiceLog";
             #endregion Setting the event log for this service
 
+            FieldInfo acceptedCommandsFieldInfo =
+                typeof(ServiceBase).GetField("acceptedCommands", BindingFlags.Instance | BindingFlags.NonPublic);
 
+            if (acceptedCommandsFieldInfo == null)
+                throw new ApplicationException("acceptedCommands field not found");
+
+            int value = (int)acceptedCommandsFieldInfo.GetValue(this);
+            acceptedCommandsFieldInfo.SetValue(this, value | SERVICE_ACCEPT_PRESHUTDOWN);
+
+        }
+
+        protected override void OnCustomCommand(int command)
+        {
+            if (command == SERVICE_CONTROL_PRESHUTDOWN)
+            {
+                Stop();
+            }
+            else
+            {
+                base.OnCustomCommand(command);
+            }
         }
 
         protected override void OnStart(string[] args)
@@ -40,6 +64,15 @@ namespace FWService
         {
             //Writing log for this event
             eventLogFwService.WriteEntry("FWService service is stopped", EventLogEntryType.Information, eventId++);
+        }
+
+        protected override void OnShutdown()
+        {
+            //Writing log for this event
+            eventLogFwService.WriteEntry("FWService service stopping is executed by OnShutdown methode.", EventLogEntryType.Information, eventId++);
+
+            Stop();
+            base.OnShutdown();
         }
     }
 }
